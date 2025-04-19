b1eb23d6810da776f3decc7444b681d595335fd8
Avinash <avigeek@outlook.in>
2025-04-20 03:45:34 +0530

    Updated APIs

--- api/config/db.js
+++ api/config/db.js
@@ -3,7 +3,11 @@ const colors = require("colors");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL);
+    await mongoose.connect(
+      process.env.MONGO_URL +
+        process.env.MONGO_DB_NAME +
+        process.env.MONGO_URL_PARAMS,
+    );
     console.log(`Mongodb connected ${mongoose.connection.host}`.bgGreen.white);
   } catch (error) {
     console.log(`Mongodb Server Issue ${error}`.bgRed.white);
